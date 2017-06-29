86c9a0eb1c166e492793a72d2a7b7f564f4ab2b0
Hiagt <hiagts@gmail.com>
2017-06-29 17:40:25 +0800

    re

--- uploads.py
+++ uploads.py
@@ -12,8 +12,6 @@ sys.setdefaultencoding('utf-8')
 
 # 上传地址
 UPLOAD_FOLDER = 'C:\Users\wz\Desktop\uploads\static\uploads'
-# 限制上传文件类别
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'folder'])
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '123'
@@ -28,8 +26,7 @@ dir_list.append(file_dir)
 
 # 判断文件后缀
 def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+    return '.' in filename
 
 
 @app.route('/')
@@ -112,53 +109,6 @@ def download_file(filename):
         return render_template('manage.html',
                                file_list=file_list,
                                size_dic=size_dic)
-
-
-@app.route('/test', methods=['GET', 'POST'])
-def create():
-    f = request.form['filename']
-    file_path = op.join(file_dir, f)
-    if op.exists(file_path):
-        flash("该目录名已存在")
-        return render_template('create_folder.html')
-    else:
-        os.makedirs(file_path)
-        flash("创建成功")
-        dir_list.append(file_path)
-        return render_template('create_folder.html')
-
-
-@app.route('/create')
-def create_folder():
-    return render_template("create_folder.html")
-
-
-# @app.route('/delete/<filename>')
-# def delete_file(filename):
-#     file_path = op.join(file_dir, filename)
-#     os.remove(file_path)
-#     return redirect(url_for('manage_file'))
-
-
-# @app.route('/search')
-# def search_file():
-#     file_list = os.listdir(file_dir)
-#     return render_template('search.html', file_list=file_list)
-
-
-# @app.route('/search/download', methods=['GET', 'POST'])
-# def search_download():
-#     file_name = request.values.getlist('files')
-#     print file_name
-#     for f in file_name:
-#     #     return send_from_directory(file_dir, f, as_attachment=True)
-#         while True:
-#             try:
-#                 urllib.urlretrieve(file_dir, f)
-#             except Exception, e:
-#                 print e
-#             else:
-#                 break
 
 
 if __name__ == '__main__':
